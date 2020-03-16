3e227b0bbb9e97f177b92cb0bdb769a3bcfbc31a
ercprk <kyoungdukp@gmail.com>
2020-03-15 21:00:11 -0400

    Separated file IO from sanitize function

--- sanitizer/sanitize.py
+++ sanitizer/sanitize.py
@@ -5,29 +5,36 @@ import re
 import html
 from sub_orgname import *
 
-nltk.download('punkt')
 
-def sanitize(data_file):
-	try:
-		file = open(data_file)
-		data = file.read()
-		split_data = nltk.tokenize.sent_tokenize(data)
-		filename = '../training_data/sanitized_data.txt'
-		for i in range(len(split_data)):
-			n_phrase = split_data[i].strip()
-			n_phrase = re.sub(r'[^\x00-\x7F]+',' ', n_phrase)
-			n_phrase = ' '.join(n_phrase.split())
-			split_data[i] = n_phrase
-		try:
-			n_file = open(filename, 'w')
-			n_file.write(str(split_data))
-		finally:
-			n_file.close()
-	finally:
-		file.close()
+
+def sanitize(text):
+
+	nltk.download('punkt')
+
+	split_data = nltk.tokenize.sent_tokenize(text)
+
+	for i in range(len(split_data)):
+		n_phrase = split_data[i].strip()
+		n_phrase = re.sub(r'[^\x00-\x7F]+',' ', n_phrase)
+		n_phrase = ' '.join(n_phrase.split())
+		split_data[i] = n_phrase
+	return str(split_data)
 
 def unescape_html(text):
 	return html.unescape(text)
 
 # file = input('File: ')
-sanitize("../training_data/marketing_phrases.txt")
+def main():
+	file = open("../training_data/marketing_phrases.txt")
+	text = file.read()
+	text = sanitize(text)
+	text = unescape_html(text)
+
+	sanitized_filename = "../training_data/sanitized_data.txt"
+	try:
+		sanitized_file = open(sanitized_filename, "w")
+		sanitized_file.write(text)
+	finally:
+		sanitized_file.close()
+
+main()
