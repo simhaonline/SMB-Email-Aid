3a0a2c8f4695246e7d444e7802117ac0f47cf2de
ercprk <kyoungdukp@gmail.com>
2020-03-21 23:40:47 -0400

    Disintegrated git lfs

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -20,11 +20,16 @@ def gpt_2_simple_train(file_name: str):
                   model_name=model_name,
                   steps=1000)    # steps is max number of training steps
 
-def gpt_2_simple_generate():
+def gpt_2_simple_generate(prefix):
 
     sess = gpt2.start_tf_sess()
     gpt2.load_gpt2(sess)
-    gpt2.generate(sess)
+    gpt2.generate(sess,
+                  length=250, # Number of tokens to generate (default 1023, max)
+                  temperature=0.7, # The higher, the crazier (default 0.7-1.0)
+                  prefix=prefix,
+                  nsamples=5,
+                  batch_size=5)
 
 #gpt_2_simple_train("marketing_phrases.txt")
-gpt_2_simple_generate()
+gpt_2_simple_generate("Starbucks")
